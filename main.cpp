1684d18822d470d149eb4db1515407732ca9ae46
ltang15 <62497601+ltang15@users.noreply.github.com>
2020-09-04 00:07:59 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * Project: Lab and plane
+ * Purpose: Using 1D and 2D library to build a lab application and a plane application
+ * Notes: Inputting the option in the menu; the sizes and required information for each program:
+            lab (number of lab, number of station and ID)
+            plane (number of row, letter of seat)
+*/
+#include <iostream>
+#include "lab.h"
+#include "plane.h"
+
+
+int main() {
+    cout << endl << endl;
+    lab_demo ();// the lab program
+    //plane_demo ();// the plan program
+    cout << endl << endl << "=========END=========" << endl << endl;
+    return 0;
+}
